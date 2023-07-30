3da485d82e1c2291fabeecb61a5154084ecc677f
Sebastian <sebas60621@gmail.com>
2023-07-29 23:02:02 -0500

    Maquetacion clases de la tienda

---
+++ moral_games.py
@@ -0,0 +1,14 @@
+class Inventario:
+    def _init_(self):
+        self.inventario = []
+
+class CarritoDeCompras:
+    pass
+
+class Cliente:
+    pass
+
+
+class Compra:
+    def _init_(self, *videojuego_comprado):
+        self.videojuego_comprado = videojuego_comprado
