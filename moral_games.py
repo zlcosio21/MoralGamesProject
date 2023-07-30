090694e7558175ea78056ab908864c5343d89015
Sebastian <sebas60621@gmail.com>
2023-07-29 23:05:38 -0500

    Registrando videojuegos al inventario de la tienda

--- moral_games.py
+++ moral_games.py
@@ -2,12 +2,21 @@ class Inventario:
     def _init_(self):
         self.inventario = []
 
+    def agregar_videojuego(self, titulo, plataforma, precio, stock, genero):
+        self.titulo = titulo
+        self.plataforma = plataforma
+        self.precio = precio
+        self.stock = stock
+        self.genero = genero
+
+        self.inventario.append(self.titulo)
+        print(f"Se ha agregado el videojuego {self.titulo} al inventario")
+
 class CarritoDeCompras:
     pass
 
 class Cliente:
     pass
-
 
 class Compra:
     def _init_(self, *videojuego_comprado):
