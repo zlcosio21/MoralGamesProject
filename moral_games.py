769c1750d68f3c55597f2e17436d7262f13c6480
Sebastian <sebas60621@gmail.com>
2023-07-30 22:42:06 -0500

    Clase para identificar clientes de la tienda

--- moral_games.py
+++ moral_games.py
@@ -1,6 +1,8 @@
 class Inventario:
     genero_videojuegos = {}
-    juegos_inventario = []
+    inventario_videojuegos = [] 
+    precio_videojuegos = {}
+    stock_videojuegos = {}
 
     def buscar_videojuego(self, videojuego_genero):
         print(f"Se busco '{videojuego_genero}', se encontro: ")
@@ -9,15 +11,15 @@ class Inventario:
             lista_generos = self.genero_videojuegos[videojuego_genero]
             for videojuego in lista_generos:
                 print(f"-{videojuego}")            
-        elif videojuego_genero in self.juegos_inventario:
+        elif videojuego_genero in self.inventario_videojuegos:
             print(f"-{videojuego_genero}")
         else:
             print(f"El videojuego o genero, no se encuentra en el inventario")
 
     def mostrar_videojuegos(self):
-        if self.juegos_inventario:
+        if self.inventario_videojuegos:
             print("Los videojuegos que se encuentran en la tienda son:")
-            for juego in self.juegos_inventario:
+            for juego in self.inventario_videojuegos:
                 print(f"-{juego}") 
         else:
             print("No hay videojuegos en el inventario de la tienda")
@@ -30,24 +32,28 @@ class Videojuego(Inventario):
         self.stock = stock  
         self.genero = genero
         
-        if self.titulo not in self.juegos_inventario:
+        if self.titulo not in self.inventario_videojuegos:
             print(f"Se ha agregado el videojuego {self.titulo} al inventario")
-            self.juegos_inventario.append(self.titulo)
+            self.inventario_videojuegos.append(self.titulo)
+            self.precio_videojuegos[self.titulo] = [self.precio]
+            self.stock_videojuegos[self.titulo] = [self.stock]            
             for genero in self.genero:
                 if genero in self.genero_videojuegos:
-                    self.genero_videojuegos[genero].append(titulo)
+                    self.genero_videojuegos[genero].append(self.titulo)
                 else:
-                    self.genero_videojuegos[genero] = [titulo]
+                    self.genero_videojuegos[genero] = [self.titulo]
         else:
             print(f"!El videojuego {self.titulo} ya se encuentra en el inventario")       
 
-class CarritoDeCompras:
-    pass
+class Cliente(Inventario):
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.productos_carrito = []
 
+    def tomar_carrito(self):
+        print(f"El Cliente {self.nombre} {self.apellido} a tomado un carrito")
 
-class Cliente:
-    pass
-
-class Compra:
+class Compra(Cliente):
     def _init_(self, *videojuego_comprado):
         self.videojuego_comprado = videojuego_comprado
